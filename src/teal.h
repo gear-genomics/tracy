0429c3b2d156231b409bbbb69f1cb698255455e4
tobiasrausch <rauschtobi@gmail.com>
2019-01-14 16:11:17 +0100

    fix header

--- src/teal.h
+++ src/teal.h
@@ -2,8 +2,6 @@
 ============================================================================
 Tracy: Trace File Handling
 ============================================================================
-Copyright (C) 2017,2018 Tobias Rausch
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
