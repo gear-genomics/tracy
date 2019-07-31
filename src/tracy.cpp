a014687dccfc93ea1d0463146a3e424f62b5540d
tobiasrausch <rauschtobi@gmail.com>
2019-07-31 13:42:31 +0200

    bsd

--- src/tracy.cpp
+++ src/tracy.cpp
@@ -1,24 +1,3 @@
-/*
-============================================================================
-Tracy: Trace File Handling
-============================================================================
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-============================================================================
-Contact: Tobias Rausch (rausch@embl.de)
-============================================================================
-*/
-
 #define _SECURE_SCL 0
 #define _SCL_SECURE_NO_WARNINGS
 #include <iostream>
@@ -76,7 +55,7 @@ int main(int argc, char **argv) {
     return 0;
   }
   else if ((std::string(argv[1]) == "license") || (std::string(argv[1]) == "--license") || (std::string(argv[1]) == "-l")) {
-    gplV3();
+    bsd();
     return 0;
   }
   else if ((std::string(argv[1]) == "index")) {
